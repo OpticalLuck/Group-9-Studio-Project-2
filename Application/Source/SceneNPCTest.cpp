6a023a424559db947434096eae7dcc8d172e4329
hqgitter <69576378+hqgitter@users.noreply.github.com>
2021-02-15 16:58:09 +0800

    Added NPC Class

---
+++ Application/Source/SceneNPCTest.cpp
@@ -0,0 +1,110 @@
+#include "SceneNPCTest.h"
+#include "GL\glew.h"
+#include "Application.h"
+#include <sstream>
+
+#include "MeshBuilder.h"
+
+SceneNPCTest::SceneNPCTest() :
+	fps(0.f)
+{
+}
+
+SceneNPCTest::~SceneNPCTest()
+{
+}
+
+void SceneNPCTest::Init()
+{
+	renderer = new Renderer();
+	//Init Meshlist
+	meshlist = new MeshList();
+	//Create Light
+	lights[0] = new Light(renderer->GetprogramID(), 0);
+	
+
+	camera.Init(Vector3(0, 3, 8), Vector3(0, 0, -1), Vector3(0, 1, 0));
+
+	Axis = goManager.CreateGO<GameObject>(meshlist->GetMesh(MeshList::MESH_AXIS));
+	Quad = goManager.CreateGO<GameObject>(meshlist->GetMesh(MeshList::MESH_QUAD));
+	NPC =  goManager.CreateGO<Character>(meshlist->GetMesh(MeshList::MESH_QUAD));
+
+	{
+	lights[0]->Set(Light::LIGHT_POINT,
+		           Vector3(0, 8, 0),
+				   Color(1, 1, 1),
+				   1.f, 1.f, 0.01f, 0.001f,
+				   Vector3(0.f, 1.f, 0.f));
+	}
+}	
+
+void SceneNPCTest::Update(double dt)
+{
+	fps = 1.f / dt;
+	static bool bLButtonState = false;
+	//Debug controls
+	{
+		if (Application::IsKeyPressed('1'))
+		{
+			glEnable(GL_CULL_FACE);
+		}
+		if (Application::IsKeyPressed('2'))
+		{
+			glDisable(GL_CULL_FACE);
+		}
+		if (Application::IsKeyPressed('3'))
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		}
+		if (Application::IsKeyPressed('4'))
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		}
+		//Input
+		if (Application::IsKeyPressed('9'))
+		{
+			Application::EnableCursor();
+		}
+		if (Application::IsKeyPressed('0'))
+		{
+			Application::DisableCursor();
+		}
+	}
+
+	camera.Updatemovement(dt);
+
+	//LMB Click
+	if (!bLButtonState && Application::IsMousePressed(0))
+	{
+		bLButtonState = true;
+	}
+	else if (bLButtonState && !Application::IsMousePressed(0))
+	{
+		bLButtonState = false;
+	}
+}
+
+void SceneNPCTest::Render()
+{
+	renderer->Reset();
+
+	//Camera
+	renderer->SetCamera(camera);
+
+
+	//Axis->Draw(renderer, false);
+	//Quad->Draw(renderer, true);
+
+	//Light
+	renderer->SetLight(lights[0]);
+	
+}
+
+void SceneNPCTest::Exit()
+{
+}
+
+void SceneNPCTest::UpdateMousePos(double xoffset, double yoffset)
+{
+	camera.Update(xoffset, yoffset);
+}
---
+++ Application/Source/SceneNPCTest.h
@@ -0,0 +1,38 @@
+#ifndef SCENE_NPCTEST_H
+#define SCENE_NPCTEST_H
+
+#include "Scene.h"
+#include "Renderer.h"
+#include "MeshList.h"
+#include "CameraVer2.h"
+#include "Light.h"
+#include "GOManager.h"
+#include "NPC.h"
+
+class SceneNPCTest : public Scene
+{
+public:
+	SceneNPCTest();
+	~SceneNPCTest();
+
+	virtual void Init();
+	virtual void Update(double dt);
+	virtual void Render();
+	virtual void Exit();
+
+	virtual void UpdateMousePos(double xoffset, double yoffset) override;
+
+private:
+	double fps;
+	Renderer* renderer;
+	MeshList* meshlist;
+	CameraVer2 camera;
+
+	GOManager goManager;
+	GameObject* Axis;
+	GameObject* Quad;
+	Character* NPC;
+	Light* lights[1];
+};
+
+#endif
