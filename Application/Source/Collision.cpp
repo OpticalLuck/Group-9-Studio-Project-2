9798b14136c2b99757dc1e87895a97001071d3ec
jeryl lah <56944877+OpticalLuck@users.noreply.github.com>
2021-02-15 09:45:12 +0800

    Problems again

--- Application/Source/Collision.cpp
+++ Application/Source/Collision.cpp
@@ -1,12 +1,17 @@
 #include "Collision.h"
 #include <sstream>
-Collision::Collision(Vector3 position, float length, float width, float height):
+
+Collision::Collision(Vector3 position, Vector3 Size):
 	position(position),
-	length(length),
-	width(width),
-	height(height)
+	Size(Size)
 {
 }
-bool Collision::TestRayOBBIntersection(Vector3 ray_origin, Vector3 ray_direction, Vector3 aabb_min, Vector3 aabb_max, Mtx44 ModelMatrix, float& intersection_distance)
+
+Collision::~Collision()
 {
+}
+
+bool Collision::CheckCollision(Collision box1, Collision box2)
+{
+	return false;
 }
--- Application/Source/Collision.h
+++ Application/Source/Collision.h
@@ -6,24 +6,13 @@ class Collision
 {
 public:
 
-	Collision(Vector3 position, float length, float width, float height);
+	Collision(Vector3 position, Vector3 Size);
 	~Collision();
 
-	bool TestRayOBBIntersection(
-		Vector3 ray_origin,        // Ray origin, in world space
-		Vector3 ray_direction,     // Ray direction (NOT target position!), in world space. Must be normalize()'d.
-		Vector3 aabb_min,          // Minimum X,Y,Z coords of the mesh when not transformed at all.
-		Vector3 aabb_max,          // Maximum X,Y,Z coords. Often aabb_min*-1 if your mesh is centered, but it's not always the case.
-		Mtx44 ModelMatrix,       // Transformation applied to the mesh (which will thus be also applied to its bounding box)
-		float& intersection_distance // Output : distance between ray_origin and the intersection with the OBB
-	);
-
-
+	bool CheckCollision(Collision box1, Collision box2);
 private:
 
 	Vector3 position;
-
-	//length = x, width = y, height = z
-	float length, width, height;
+	Vector3 Size;
 };
 
