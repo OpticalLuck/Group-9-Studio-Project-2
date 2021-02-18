f4203e0c8e08a5e65edadd27e29c5f12f0c98d1b
hqgitter <69576378+hqgitter@users.noreply.github.com>
2021-02-18 13:07:30 +0800

    Formatting Meshlist

--- Application/Source/MeshList.cpp
+++ Application/Source/MeshList.cpp
@@ -3,19 +3,23 @@
 #include "LoadTGA.h"
 MeshList::MeshList()
 {
+	//// <PRIMATIVES> 
 	meshlist[MESH_AXIS] = MeshBuilder::GenerateAxes("Axis", 1000, 1000, 1000);
 
 	meshlist[MESH_QUAD] = MeshBuilder::GenerateQuad("FloorQuad", Color(1, 1, 1));
 
 	meshlist[MESH_CUBE] = MeshBuilder::GenerateCube("Cube", Color(1.f, 1.f, 1.f), 1.f, 1.f, 1.f);
+	//// </PRIMATIVES> 
 
+	//// <UI> 
 	meshlist[MESH_STAMINABAR] = MeshBuilder::GenerateCube("Cube", Color(0.f, 1.f, 0.f), 1.f, 1.f, 1.f);
 
 	meshlist[MESH_ICON] = MeshBuilder::GenerateQuad("Icon", Color(1.f, 0.f, 0.f), 1.0f);
+	//// </UI> 
 
+	//// <NPC> 
 	meshlist[MESH_HEAD] = MeshBuilder::GenerateCube("Head", Color(1.f,1.f,0.f), 1.f, 1.f, 1.f);
-
-	meshlist[MESH_CUBE] = MeshBuilder::GenerateCube("Cube", Color(1.f,1.f,1.f), 1.f, 1.f, 1.f);
+	//// </NPC> 
 }
 
 MeshList::~MeshList()
