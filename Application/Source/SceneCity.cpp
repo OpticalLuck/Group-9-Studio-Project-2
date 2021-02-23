5c1ee45c276ab44687242e278691cd3f0922aa61
hqgitter <69576378+hqgitter@users.noreply.github.com>
2021-02-23 13:39:29 +0800

    Set it back to normal nontesting version

--- Application/Source/SceneCity.cpp
+++ Application/Source/SceneCity.cpp
@@ -48,7 +48,7 @@ void SceneCity::Init()
 	Ayaka = goManager.CreateGO<Character>(meshlist->GetMesh(MeshList::MESH_AYAKA));
 	Ayaka->Init(Vector3(0, 0, 5), Vector3(0, 0, 0), Vector3(0.2f, 0.2f, 0.2f));
 	Ayaka->SetRotate(Vector3(0,Math::RadianToDegree(atan2(camera.GetView().x, camera.GetView().z)) ,0));
-	Ayaka->SetColliderBox( Vector3(2, 0.2, 2), Vector3(0,0,0) ); //foot box (always first)
+	//Ayaka->SetColliderBox( Vector3(2, 0.2, 2), Vector3(0,0,0) ); //foot box (always first)
 	Ayaka->SetColliderBox(Vector3(0.8f, 2.f, 0.8f), Vector3(0, 2, 0));
 
 	//std::cout << Ayaka->GetColliderBox(0)->GetPos().x << "\n" ;
